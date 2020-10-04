786f284e76cfd02b9557c01aeadb6f176bc1af49
dexagod <Ruben.Dedecker@Ugent.be>
2020-10-04 18:38:55 +0200

    Improved React routing

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import HelpContainerComponent from './Components/HelpContainerComponent'
 import * as serviceWorker from './serviceWorker';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 ReactDOM.render(
+  
   <React.StrictMode>
     <Router>
-      <Route exact path="/" component={App} />
-      <Route exact path="/help" component={HelpContainerComponent} />
+      <Switch>
+        <Route path="/help">
+          <HelpContainerComponent />
+        </Route>
+        {/* The default route */}
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
